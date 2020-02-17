4dc9bd2883166d8e82921435786bff33d8f720a9
MarkNjunge <mark.kamau@outlook.com>
2020-02-17 19:00:36 +0300

    Fix going back to splash activity

--- app/src/main/java/com/marknjunge/ledger/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/marknjunge/ledger/ui/splash/SplashActivity.kt
@@ -11,5 +11,6 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
